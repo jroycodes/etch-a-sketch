5c726ced68b02b2a2b4c37093f8b341ab0e9a3ce
Jonathan Roy <jonaisroy@gmail.com>
2024-04-17 16:08:54 +0300

    New feature [Random color squares]

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 "use strict";
 
+function addRgb(e) {
+  const red = Math.floor(Math.random() * 10);
+  const green = Math.floor(Math.random() * 10);
+  const blue = Math.floor(Math.random() * 256);
+  e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+  e.target.removeEventListener("pointerover", addRgb);
+}
+
 function defaultGrid() {
-  // select element with class container
   const container = document.querySelector("#container");
 
-  // create a 16*16 grid of square divs
   for (let i = 0; i < 16 * 16; i++) {
     const allDivs = document.createElement("div");
     allDivs.style.height = "1rem";
     allDivs.style.width = "1rem";
     allDivs.style.border = "1px solid black";
-    allDivs.addEventListener("mouseenter", () => {
-      allDivs.style.backgroundColor = "yellow";
-    });
-    // change BC to blueViolet when mouse leaves div
-    allDivs.addEventListener("mouseleave", () => {
-      allDivs.style.backgroundColor = "blueViolet";
-    });
+    allDivs.addEventListener("pointerover", addRgb); 
     container.appendChild(allDivs);
   }
 }
 
-// Change Grid
 const gridChanger = document.getElementById("square_num");
 gridChanger.style.backgroundColor = "black";
 gridChanger.style.color = "yellow";
 gridChanger.addEventListener("click", () => {
   let column = prompt("Insert number of squares for new grid");
   if (column > 0 && column <= 100) {
-    // Clear existing grid
+    const container = document.querySelector("#container");
     container.innerHTML = "";
     customGrid(column);
   } else alert("Number must be between 0 - 100");
 });
 
 function customGrid(column) {
-  // select element with class container
   const container = document.querySelector("#container");
 
   for (let i = 0; i < column * column; i++) {
@@ -43,14 +41,65 @@ function customGrid(column) {
     allDivs.style.height = "1rem";
     allDivs.style.width = "1rem";
     allDivs.style.border = "1px solid black";
-    allDivs.addEventListener("mouseenter", () => {
-      allDivs.style.backgroundColor = "yellow";
-    });
-    // change BC to blueViolet when mouse leaves div
-    allDivs.addEventListener("mouseleave", () => {
-      allDivs.style.backgroundColor = "blueViolet";
-    });
+    allDivs.addEventListener("pointerover", addRgb);
     container.appendChild(allDivs);
   }
 }
+
 defaultGrid();
+
+
+// function defaultGrid() {
+//   // select element with class container
+//   const container = document.querySelector("#container");
+
+//   // create a 16*16 grid of square divs
+//   for (let i = 0; i < 16 * 16; i++) {
+//     const allDivs = document.createElement("div");
+//     allDivs.style.height = "1rem";
+//     allDivs.style.width = "1rem";
+//     allDivs.style.border = "1px solid black";
+//     allDivs.addEventListener("mouseenter", () => {
+//       allDivs.style.backgroundColor = "yellow";
+//     });
+//     // change BC to blueViolet when mouse leaves div
+//     allDivs.addEventListener("mouseleave", () => {
+//       allDivs.style.backgroundColor = "blueViolet";
+//     });
+//     container.appendChild(allDivs);
+//   }
+// }
+
+// // Change Grid
+// const gridChanger = document.getElementById("square_num");
+// gridChanger.style.backgroundColor = "black";
+// gridChanger.style.color = "yellow";
+// gridChanger.addEventListener("click", () => {
+//   let column = prompt("Insert number of squares for new grid");
+//   if (column > 0 && column <= 100) {
+//     // Clear existing grid
+//     container.innerHTML = "";
+//     customGrid(column);
+//   } else alert("Number must be between 0 - 100");
+// });
+
+// function customGrid(column) {
+//   // select element with class container
+//   const container = document.querySelector("#container");
+
+//   for (let i = 0; i < column * column; i++) {
+//     const allDivs = document.createElement("div");
+//     allDivs.style.height = "1rem";
+//     allDivs.style.width = "1rem";
+//     allDivs.style.border = "1px solid black";
+//     allDivs.addEventListener("mouseenter", () => {
+//       allDivs.style.backgroundColor = "yellow";
+//     });
+//     // change BC to blueViolet when mouse leaves div
+//     allDivs.addEventListener("mouseleave", () => {
+//       allDivs.style.backgroundColor = "blueViolet";
+//     });
+//     container.appendChild(allDivs);
+//   }
+// }
+// defaultGrid();
