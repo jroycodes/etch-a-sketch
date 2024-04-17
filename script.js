adf29167aca0b1975c79774116a68dd847f2998e
Jonathan Roy <jonaisroy@gmail.com>
2024-04-17 10:05:10 +0300

    created a 16*16 grid of square divs

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 "use strict";
 
 // select element with class container
-const container = document.querySelector(".container");
+const container = document.querySelector("#container");
+
 // create a 16*16 grid of square divs
-const allDivs = doucment.createElement("div");
-// add class
-allDivs.classList.add("content");
-// place the new element into the DOM
-container.appendChild(allDivs);
+let allDivs;
+let divs = 16;
 
-const pContainer = document.querySelector(".container");
+for (let i = 0; i < divs*divs; i++) {
+  allDivs = document.createElement("div");
+  allDivs.setAttribute("id", "theDiv");
+  allDivs.style.height = "1rem";
+  allDivs.style.width = "1rem";
+  allDivs.style.border = "1px solid black";
+//   allDivs.style.backgroundColor = "orangeRed";
+  container.appendChild(allDivs);
+}
 
-const paragraph = document.createElement("p");
-paragraph.textContent = "Hey I'm red!";
-paragraph.style.color = "red";
 
-pContainer.appendChild(paragraph);
+// const btn = document.querySelector("#container");
+
+// function random(number) {
+//   return Math.floor(Math.random() * (number + 1));
+// }
+
+// function changeBackground() {
+//   const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+//   document.body.style.backgroundColor = rndCol;
+// }
+
+// btn.addEventListener("click", changeBackground);
